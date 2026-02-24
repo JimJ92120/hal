db4d97cf74d277e5e67dc4d4178d34a9a4fda89b
JimJ92120 <jimj.development@gmail.com>
2026-02-24 21:53:25 +0700

    feat(examples): test print values from lib

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,5 +1,13 @@
-use hal_lib::hello_world;
+#![allow(dead_code)]
+
+use hal_lib::{
+    globals::{ Register as RegisterTrait },
+    registers::atmega328p::PORTB as Register
+};
 
 fn main() {
-    println!("{}", hello_world());
+    println!("register: {:?}", Register {});
+    println!("address: {:?}", Register::ADDRESS);
+    println!("bit value: {:x}", Register::PB2 as u8);
+    println!("bit mask: {:08b}", Register::PB2.mask());
 }
