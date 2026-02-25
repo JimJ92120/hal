8d3339f7b4bfe77bf9333dcbd8630cb18aced7e3
JimJ92120 <jimj.development@gmail.com>
2026-02-25 23:28:05 +0700

    refactor(lib > boards > arduino uno): remove pin to registrers dependency, match to correct registers bitfield (to refactor redundant code and matches)

--- lib/src/boards/arduino_uno/gpio/mod.rs
+++ lib/src/boards/arduino_uno/gpio/mod.rs
@@ -1,9 +1,9 @@
 use crate::{
-    globals::{ Bit, Register },
+    globals::{ Register },
     registers::atmega328p::{
-        DDRB, PORTB,
-        DDRC, PORTC,
-        DDRD, PORTD,
+        DDRB, DDRBBitField, PORTB, PORTBBitField,
+        DDRC, DDRCBitField, PORTC, PORTCBitField,
+        DDRD, DDRDBitField, PORTD, PORTDBitField
     }
 };
 
@@ -16,78 +16,104 @@ pub struct GPIO;
 
 impl GPIO {
     pub fn set_output(pin: Pin) {
-        let bit_mask = pin.bit().mask();
+        let bit = Self::get_ddr_bit_field(&pin);
 
         match pin {
             Pin::Zero | Pin::One | Pin::Two | Pin::Three | Pin::Four | Pin::Five | Pin::Six | Pin::Seven
-                => unsafe {
-                    DDRD::set(bit_mask);
-                },
+                => DDRD::set_bit_mask(bit),
             Pin::Eight | Pin::Nine | Pin::Ten | Pin::Eleven | Pin::Twelve | Pin::Thirteen
-                => unsafe {
-                    DDRB::set(bit_mask);
-                },
+                => DDRB::set_bit_mask(bit),
             Pin::Fourteen | Pin::Fifteen | Pin::Sixteen | Pin::Seventeen | Pin::Eighteen | Pin::Nineteen
-                => unsafe {
-                    DDRC::set(bit_mask);
-                },
+                => DDRC::set_bit_mask(bit),
         };
     }
 
     pub fn set_input(pin: Pin) {
-        let bit_mask = pin.bit().mask();
+        let bit = Self::get_ddr_bit_field(&pin);
 
         match pin {
             Pin::Zero | Pin::One | Pin::Two | Pin::Three | Pin::Four | Pin::Five | Pin::Six | Pin::Seven
-                => unsafe {
-                    DDRD::set(DDRD::get() & !bit_mask);
-                },
+                => DDRD::unset_bit_mask(bit),
             Pin::Eight | Pin::Nine | Pin::Ten | Pin::Eleven | Pin::Twelve | Pin::Thirteen
-                => unsafe {
-                    DDRD::set(DDRD::get() & !bit_mask);
-                },
+                => DDRB::unset_bit_mask(bit),
             Pin::Fourteen | Pin::Fifteen | Pin::Sixteen | Pin::Seventeen | Pin::Eighteen | Pin::Nineteen
-                => unsafe {
-                    DDRD::set(DDRD::get() & !bit_mask);
-                },
+                => DDRC::unset_bit_mask(bit),
         };
     }
 
     pub fn set_high(pin: Pin) {
-        let bit_mask = pin.bit().mask();
+        let bit = Self::get_port_bit_field(&pin);
 
         match pin {
             Pin::Zero | Pin::One | Pin::Two | Pin::Three | Pin::Four | Pin::Five | Pin::Six | Pin::Seven
-                => unsafe {
-                    PORTD::set(bit_mask);
-                },
+                => PORTD::set_bit_mask(bit),
             Pin::Eight | Pin::Nine | Pin::Ten | Pin::Eleven | Pin::Twelve | Pin::Thirteen
-                => unsafe {
-                    PORTB::set(bit_mask);
-                },
+                => PORTB::set_bit_mask(bit),
             Pin::Fourteen | Pin::Fifteen | Pin::Sixteen | Pin::Seventeen | Pin::Eighteen | Pin::Nineteen
-                => unsafe {
-                    PORTC::set(bit_mask);
-                },
+                => PORTC::set_bit_mask(bit),
         };
     }
 
     pub fn set_low(pin: Pin) {
-        let bit_mask = pin.bit().mask();
+        let bit = Self::get_port_bit_field(&pin);
 
         match pin {
             Pin::Zero | Pin::One | Pin::Two | Pin::Three | Pin::Four | Pin::Five | Pin::Six | Pin::Seven
-                => unsafe {
-                    PORTD::set(PORTD::get() & !bit_mask);
-                },
+                => PORTD::unset_bit_mask(bit),
             Pin::Eight | Pin::Nine | Pin::Ten | Pin::Eleven | Pin::Twelve | Pin::Thirteen
-                => unsafe {
-                    PORTB::set(PORTB::get() & !bit_mask);
-                },
+                => PORTB::unset_bit_mask(bit),
             Pin::Fourteen | Pin::Fifteen | Pin::Sixteen | Pin::Seventeen | Pin::Eighteen | Pin::Nineteen
-                => unsafe {
-                    PORTC::set(PORTC::get() & !bit_mask);
-                },
+                => PORTC::unset_bit_mask(bit),
         };
+    }
+    
+    fn get_ddr_bit_field(pin: &Pin) -> u8 {
+        match pin {
+            Pin::Zero       => DDRDBitField::PD0 as u8,
+            Pin::One        => DDRDBitField::PD1 as u8,
+            Pin::Two        => DDRDBitField::PD2 as u8,
+            Pin::Three      => DDRDBitField::PD3 as u8,
+            Pin::Four       => DDRDBitField::PD4 as u8,
+            Pin::Five       => DDRDBitField::PD5 as u8,
+            Pin::Six        => DDRDBitField::PD6 as u8,
+            Pin::Seven      => DDRDBitField::PD7 as u8,
+            Pin::Eight      => DDRBBitField::PB0 as u8,
+            Pin::Nine       => DDRBBitField::PB1 as u8,
+            Pin::Ten        => DDRBBitField::PB2 as u8,
+            Pin::Eleven     => DDRBBitField::PB3 as u8,
+            Pin::Twelve     => DDRBBitField::PB4 as u8,
+            Pin::Thirteen   => DDRBBitField::PB5 as u8,
+            Pin::Fourteen   => DDRCBitField::AC0 as u8,
+            Pin::Fifteen    => DDRCBitField::AC1 as u8,
+            Pin::Sixteen    => DDRCBitField::AC2 as u8,
+            Pin::Seventeen  => DDRCBitField::AC3 as u8,
+            Pin::Eighteen   => DDRCBitField::AC4 as u8,
+            Pin::Nineteen   => DDRCBitField::AC5 as u8,
+        }
+    }
+
+    fn get_port_bit_field(pin: &Pin) -> u8 {
+        match pin {
+            Pin::Zero       => PORTDBitField::PD0 as u8,
+            Pin::One        => PORTDBitField::PD1 as u8,
+            Pin::Two        => PORTDBitField::PD2 as u8,
+            Pin::Three      => PORTDBitField::PD3 as u8,
+            Pin::Four       => PORTDBitField::PD4 as u8,
+            Pin::Five       => PORTDBitField::PD5 as u8,
+            Pin::Six        => PORTDBitField::PD6 as u8,
+            Pin::Seven      => PORTDBitField::PD7 as u8,
+            Pin::Eight      => PORTBBitField::PB0 as u8,
+            Pin::Nine       => PORTBBitField::PB1 as u8,
+            Pin::Ten        => PORTBBitField::PB2 as u8,
+            Pin::Eleven     => PORTBBitField::PB3 as u8,
+            Pin::Twelve     => PORTBBitField::PB4 as u8,
+            Pin::Thirteen   => PORTBBitField::PB5 as u8,
+            Pin::Fourteen   => PORTCBitField::AC0 as u8,
+            Pin::Fifteen    => PORTCBitField::AC1 as u8,
+            Pin::Sixteen    => PORTCBitField::AC2 as u8,
+            Pin::Seventeen  => PORTCBitField::AC3 as u8,
+            Pin::Eighteen   => PORTCBitField::AC4 as u8,
+            Pin::Nineteen   => PORTCBitField::AC5 as u8,
+        }
     }
 }
--- lib/src/boards/arduino_uno/gpio/pin.rs
+++ lib/src/boards/arduino_uno/gpio/pin.rs
@@ -1,8 +1,3 @@
-use crate::{
-    globals::Bit,
-    registers::atmega328p::{ PORTB, PORTC, PORTD }
-};
-
 #[derive(Debug)]
 pub enum Pin {
     Zero,
@@ -25,31 +20,4 @@ pub enum Pin {
     Seventeen,
     Eighteen,
     Nineteen,
-}
-
-impl Pin {
-    pub fn bit(&self) -> Bit {
-        match self {
-            Pin::Zero       => PORTD::PD0,
-            Pin::One        => PORTD::PD1,
-            Pin::Two        => PORTD::PD2,
-            Pin::Three      => PORTD::PD3,
-            Pin::Four       => PORTD::PD4,
-            Pin::Five       => PORTD::PD5,
-            Pin::Six        => PORTD::PD6,
-            Pin::Seven      => PORTD::PD7,
-            Pin::Eight      => PORTB::PB0,
-            Pin::Nine       => PORTB::PB1,
-            Pin::Ten        => PORTB::PB2,
-            Pin::Eleven     => PORTB::PB3,
-            Pin::Twelve     => PORTB::PB4,
-            Pin::Thirteen   => PORTB::PB5,
-            Pin::Fourteen   => PORTC::AC0,
-            Pin::Fifteen    => PORTC::AC1,
-            Pin::Sixteen    => PORTC::AC2,
-            Pin::Seventeen  => PORTC::AC3,
-            Pin::Eighteen   => PORTC::AC4,
-            Pin::Nineteen   => PORTC::AC5,
-        }
-    }
 }
